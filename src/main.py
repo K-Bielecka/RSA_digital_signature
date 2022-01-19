2a8d9a0afeda6582534baf15ce517a2ef89ad238
C-url <kasia.bielecka999@gmail.com>
2022-01-19 18:10:13 +0100

    clean code

--- src/main.py
+++ src/main.py
@@ -24,8 +24,11 @@ def generate_keys_to_file():
     return ((n,e),d)
 
 def generate_signature_to_file(m, n, e, d):
+    #get path and generate signature
     signature_path = input("Please provide path for signature file \n")
     s = generate_signature(m,n,e,d)
+
+    #verify signature
     is_valid = verify(m,n,e,s)
     if(is_valid):
         #save signature to file
@@ -40,6 +43,7 @@ def get_keys_from_files():
     #get paths to keys
     public_key_path = input("Please provide path to your public key\n")
     private_key_path = input("Please provide path to your private key\n")
+
     #get keys
     with open(str(public_key_path),'r') as reader:
         file_contents = reader.read()
@@ -54,34 +58,45 @@ def user_input_switch(key: int):
         case 1:
             #generate keys option
             print("You chose option: " + option1)  
-            user_generate_keys_to_file() 
+            generate_keys_to_file() 
+
         case 2:
             #generate signature for input message
             print("You chose option: " + option2)
+
             #get message
             message = input("Please type the message to be signed \n")
+
             # get keys
             ((n,e),d) = get_keys_from_files()
+
             #generate signature and save to file
-            user_generate_signature_to_file(message,int(n),int(e),int(d))
+            generate_signature_to_file(message,int(n),int(e),int(d))
+
         case 3:
             #generate signature for message from given file
             print("You chose option: " + option3)  
+
             #get file with message path
             message_path = input("Please provide path to the file with the message, that you want to sign \n")
+            
             #read message  
             with open(str(message_path),'r') as reader:
                message = reader.read()
+            
             #get keys
             ((n,e),d) = get_keys_from_files()
-            user_generate_signature_to_file(message,int(n),int(e),int(d))
+            generate_signature_to_file(message,int(n),int(e),int(d))
+
         case 4:
             #verify signature from given file
             print("You chose option: " + option4)
+
              #get file with message path
             signature_path = input("Please provide path to the file with the signature, that you want to verify \n")
             message_path = input("Please provide path to the file with message signed with your signature \n")
             public_key_path = input("Please provide path to your public key\n")
+
             #read message  
             with open(str(signature_path),'r') as reader:
                signature = reader.read()
@@ -90,45 +105,17 @@ def user_input_switch(key: int):
             with open(str(public_key_path),'r') as reader:
                file_contents = reader.read()
             (n,e) = file_contents.split(',')
+
             #verify signature and return result info
             is_valid = verify(message,int(n),int(e),int(signature))
             if(is_valid):
                 print("Provided signature is valid. \n")
             else:
                 print("Provided signature is invalid.\n")
-                
+
         case _:
             print("Please choose a valid option 1-4")
 
 print(welcome_text, option_list)
 user_option_input = int(input("Type 1, 2, 3 or 4 to choose the option and press 'Enter'. \n"))
-
 user_input_switch(user_option_input)
-
-#Think about implementing the option to "escape" the certain option if for example somoene made a mistake. Should be done possibly with event listener, catch the 
-#  key press event and exit if it is the 'Escape' key
-
-#Below is code that might help with this
-
-# from pynput import keyboard
-
-# def on_press(key):
-#     try:
-#         print('alphanumeric key {0} pressed'.format(
-#             key.char))
-#     except AttributeError:
-#         print('special key {0} pressed'.format(
-#             key))
-
-# def on_release(key):
-#     print('{0} released'.format(
-#         key))
-#     if key == keyboard.Key.esc:
-#         # Stop listener
-#         return False
-
-# # Collect events until released
-# with keyboard.Listener(
-#         on_press=on_press,
-#         on_release=on_release) as listener:
-#     listener.join()
